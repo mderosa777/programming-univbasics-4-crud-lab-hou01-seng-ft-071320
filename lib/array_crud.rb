7e5df095623a7ce476af739d1332a758334b0773
megan <meggsnbacon777@gmail.com>
2020-07-03 03:50:45 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,13 +6,11 @@ def create_an_array
  p create_an_array
   end
 def add_element_to_end_of_array(array,element)
-
-  array.push(element)
+ array.push(element)
 end
 def add_element_to_start_of_array(array, element)
  array.unshift(element) 
 end
-
 def remove_element_from_end_of_array(array)
  array.pop 
 end
@@ -26,11 +24,11 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  first(Array)
+  array.first
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array.last
 end
 
 def update_element_from_index(array, index_number, element)
